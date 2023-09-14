48a5e353976b99573d342139a0c9607a446d7308
guilhermegoebel <guilhermegoebelifpr@gmail.com>
2023-09-14 15:04:00 -0300

    test

--- src/db/migrations/1694714198926-NoivaRefactoring.ts
+++ src/db/migrations/1694714198926-NoivaRefactoring.ts
@@ -1,18 +1,15 @@
 import { MigrationInterface, QueryRunner } from "typeorm"
 
-export class NoivaRefactoring1694470814389 implements MigrationInterface {
-
-    public async up(queryRunner: QueryRunner): Promise<void> {
+export class PostRefactoring1694714198926 implements MigrationInterface {
+    async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(
             `ALTER TABLE "noiva" RENAME COLUMN "contact" TO "email"`,
         )
     }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
+    async down(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(
             `ALTER TABLE "noiva" RENAME COLUMN "email" TO "contact"`,
         ) 
     }
-
 }
-
